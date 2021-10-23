b794a70c818cd8056a7f0a8573d3996f245bda41
philippadviqo <philipp.schmalen@adviqo.com>
2021-10-23 18:00:45 +0200

    #1my697u added fig

--- app.py
+++ app.py
@@ -39,30 +39,37 @@ fraud_value = c3.slider(
 # "---"
 
 c4 = st.container()
-# c4_cols = c4.columns(2)
 c4.subheader("Data quality")
-data_quality_options = ["great", "solid", "basic"]
-data_quality_label = c4.radio(label="", options=data_quality_options)
+data_quality_labels = ["basic", "solid", "great"]
+data_quality = (
+    c4.radio(
+        label="",
+        options=range(len(data_quality_labels)),
+        format_func=lambda x: data_quality_labels[x],
+    )
+    + 1
+)
 st.write(
     "<style>div.row-widget.stRadio > div{flex-direction:row;}</style>",
     unsafe_allow_html=True,
 )
 
-
 "---"
 
-savings_daily = (events * (fraud_rate / 100) * fraud_value) / len(
-    data_quality_options
+savings_daily = (events * (fraud_rate / 100) * fraud_value) * (
+    data_quality / len(data_quality_labels)
 )
 savings_annual = savings_daily * 365
-df_len = len(data_quality_options)
 
-st.header(f"Expect {savings_annual:,.0f}€ annual savings")
+st.header(f"Save each day up to {savings_daily:,.0f}€")
+st.header(f"Estiamted savings ~{savings_annual:,.0f}€ annually")
 
+
+df_len = len(data_quality_labels)
 df = pd.DataFrame(
     {
         "events": [events] * df_len,
-        "data_quality_label": data_quality_options,
+        "data_quality_label": data_quality_labels,
         "data_quality": range(1, df_len + 1),
         "fraud_value": [fraud_value] * df_len,
         "fraud_rate": [fraud_rate] * df_len,
@@ -71,9 +78,20 @@ df = pd.DataFrame(
     annual_savings=lambda x: (
         (x.events * (x.fraud_rate / 100)) * x.fraud_value * 365
     )
-    / (x.data_quality / 3)
+    * (x.data_quality / df_len)
 )
 "", df
 
 
-# fig = px.area()
+fig = px.bar(
+    df,
+    x="data_quality_label",
+    y="annual_savings",
+    color="data_quality_label",
+    labels={
+        "data_quality_label": "Data quality",
+        "annual_savings": "Annual savings",
+    },
+)
+
+st.plotly_chart(fig)
