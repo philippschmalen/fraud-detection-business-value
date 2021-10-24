b872a13a5d6760e1206ea7bfb5bb2a22f5927c43
philippadviqo <philipp.schmalen@adviqo.com>
2021-10-24 21:10:25 +0200

    omit plot

--- app.py
+++ app.py
@@ -92,32 +92,32 @@ df = create_df_business_value(
 )
 
 # plot
-fig = px.bar(
-    df,
-    x="data_quality_label",
-    y="annual_savings",
-    color="data_quality_label",
-    labels={
-        "data_quality_label": "Datenqualität",
-        "annual_savings": "Jährliche Ersparnis",
-    },
-    text="annual_savings_label",
-    template="simple_white",
-)
-fig.update_layout(
-    showlegend=False, xaxis_visible=False, yaxis_visible=False, font_size=14
-)
+# fig = px.bar(
+#     df,
+#     x="data_quality_label",
+#     y="annual_savings",
+#     color="data_quality_label",
+#     labels={
+#         "data_quality_label": "Datenqualität",
+#         "annual_savings": "Jährliche Ersparnis",
+#     },
+#     text="annual_savings_label",
+#     template="simple_white",
+# )
+# fig.update_layout(
+#     showlegend=False, xaxis_visible=False, yaxis_visible=False, font_size=14
+# )
 
-fig.update_traces(
-    hovertemplate="Datenqualität: %{x} <br> Bis zu €%{y:,.0f} einsparen"
-)
-fig.add_annotation(
-    text="Jährliche Ersparnis <br>je nach Datenqualität",
-    xref="paper",
-    yref="paper",
-    x=0.05,
-    y=0.95,
-    showarrow=False,
-)
+# fig.update_traces(
+#     hovertemplate="Datenqualität: %{x} <br> Bis zu €%{y:,.0f} einsparen"
+# )
+# fig.add_annotation(
+#     text="Jährliche Ersparnis <br>je nach Datenqualität",
+#     xref="paper",
+#     yref="paper",
+#     x=0.05,
+#     y=0.95,
+#     showarrow=False,
+# )
 
-st.plotly_chart(fig, use_container_width=True)
+# st.plotly_chart(fig, use_container_width=True)
